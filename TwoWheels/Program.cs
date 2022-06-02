be5574c18c9d66520f2a7700cbcaab6e84eee8ba
Clint Kingston <ckingston1@woolworths.com.au>
2022-06-03 08:01:44 +1000

    first commit

--- TwoWheels/Program.cs
+++ TwoWheels/Program.cs
@@ -4,17 +4,14 @@ using System.Drawing;
 using Console = Colorful.Console;
 using JsonSubTypes;
 
-namespace TwoWheels // Note: actual namespace depends on the project name.
+namespace TwoWheels
 {
     internal class Program
     {
-
-
         public static List<Products> theCatalog = new List<Products>();
         public static string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Catalogs");
         public static string filePath = Path.Combine(folderPath, "Default_Catalog.json");
         public static string readData = File.ReadAllText(Path.Combine(folderPath, "Default_Catalog.json"));
-
 
         static void Main(string[] args)
         {
